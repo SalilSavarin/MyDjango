7b3ac2f3bf2a4c50a51f9787033db0fb42e720e7
SalilSavarin <yan-sav@yandex.ru>
2023-01-11 20:44:29 +0300

    Ex Models list displaying

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,47 @@
 from django.shortcuts import render
+from .models import Book
 
 
 def books_view(request):
+    books_from_db = Book.objects.all()
+    books = list()
+    for book in books_from_db:
+        books.append({
+            'name': book.name,
+            'author': book.author,
+            'pub_date': book.pub_date.strftime('%d-%m-%Y')
+        })
+        day, month, year = book.pub_date.strftime('%d-%m-%Y').split('-')
     template = 'books/books_list.html'
-    context = {}
+    context = {'books': books}
     return render(request, template, context)
+
+
+def book_view(request, year, month, day):
+    contex = dict()
+    books_from_db = Book.objects.all()
+    books = list()
+    for book in books_from_db:
+        if f'{year}-{month}-{day}' == book.pub_date.strftime('%Y-%m-%d'):
+            books.append({
+                'name': book.name,
+                'author': book.author,
+                'pub_date': book.pub_date.strftime('%d-%m-%Y')
+            })
+    dates = Book.objects.values_list('pub_date', flat=True).distinct().order_by('pub_date')
+    dates = [date.strftime('%Y-%m-%d')for date in dates]
+    current_date_index = dates.index(f'{year}-{month}-{day}')
+    if current_date_index != 0:
+        contex['has_previous_date'] = True
+        contex['previous_pub_date'] = dates[current_date_index - 1]
+    if current_date_index != len(dates) - 1:
+        contex['has_next_date'] = True
+        contex['next_pub_date'] = dates[current_date_index + 1]
+    contex['books'] = books
+
+    return render(request, 'books/books_list.html', context=contex)
+
+
+def home(request):
+    template = 'base.html'
+    return render(request, template)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from books.views import books_view
+from books.views import books_view, home, book_view
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', home, name='home'),
+    path('books/<str:year>-<str:month>-<str:day>', book_view, name='book_date'),
+    path('books/', books_view, name='books'),
     path('admin/', admin.site.urls),
+
 ]

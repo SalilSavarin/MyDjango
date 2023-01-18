c7319a4732e0ae6d9a20a7fad1f402e0ac5f354d
SalilSavarin <yan-sav@yandex.ru>
2023-01-18 19:55:13 +0300

    Ex 2 almost

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Scope, Tag
 
+
+class ArticleScopeInLine(admin.TabularInline):
+    model = Scope
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInLine]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 
 
-class Article(models.Model):
+class Tag(models.Model):
+    name = models.CharField(max_length=40, verbose_name='Название')
 
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+
+class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +24,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    is_main = models.BooleanField(default=False)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')

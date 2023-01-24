97e75abfa42f587293a91247ae246a4eba55166b
SalilSavarin <yan-sav@yandex.ru>
2023-01-24 21:51:19 +0300

    Ex2 m2m-relations

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,10 +1,26 @@
 from django.contrib import admin
 
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
 from .models import Article, Scope, Tag
 
 
+class ArticleScopeInLineFormset(BaseInlineFormSet):
+    def clean(self):
+        is_main_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                is_main_count += 1
+                if is_main_count > 1:
+                    raise ValidationError('Основной тег может быть только один')
+            if is_main_count == 0:
+                raise ValidationError('Обязательно нужен основной тег')
+        return super().clean()
+
 class ArticleScopeInLine(admin.TabularInline):
     model = Scope
+    formset = ArticleScopeInLineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -21,6 +21,7 @@ class Article(models.Model):
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
@@ -30,3 +31,6 @@ class Scope(models.Model):
     is_main = models.BooleanField(default=False)
     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+
+    class Meta:
+        ordering = ['-is_main', 'tag__name']
